2c8738be6d45d9fd7ace3970c6ded22917ef62a9
Daniel Šebesta <dast@duck.com>
2025-08-04 09:24:13 +0200

    Update worker.js

--- worker.js
+++ worker.js
@@ -26,12 +26,12 @@ async function fetch(request, env) {
   console.log('Country:', request.cf?.country)
   console.log('Accept-Language:', request.headers.get('accept-language'))
   
-  let ip
+  let ipData
   if (env.isDev) {
-    ip = generateRandomIpv4OrIpv6Address()
+    ipData = generateRandomIpData()
   }
   else {
-    ip = getIpAddress(request)
+    ipData = getIpData(request)
   }
   
   let body = ''
@@ -41,7 +41,7 @@ async function fetch(request, env) {
   headers.append('Strict-Transport-Security', 'max-age=33333333; includeSubDomains; preload')
   switch (pathname) {
     case '/':
-      body = transform(indexHtml, ip, getLang(request.headers.get('accept-language')))
+      body = transform(indexHtml, ipData, getLang(request.headers.get('accept-language')))
       break
     case '/translate':
       body = translateHtml
@@ -56,8 +56,8 @@ async function fetch(request, env) {
   })
 }
 
-function getIpAddress(request) {
-  // Zkusíme získat všechny možné IP adresy z různých headerů
+function getIpData(request) {
+  // Získáme všechny možné IP adresy z různých headerů
   const cfConnectingIp = request.headers.get('cf-connecting-ip')
   const xForwardedFor = request.headers.get('x-forwarded-for')
   const xRealIp = request.headers.get('x-real-ip')
@@ -78,18 +78,28 @@ function getIpAddress(request) {
   
   console.log('All available IPs:', validIps)
   
-  // Najdeme první IPv4 adresu
+  // Najdeme IPv4 a IPv6 adresy
+  let ipv4 = null
+  let ipv6 = null
+  
   for (const ip of validIps) {
-    if (isIPv4(ip)) {
-      console.log('Selected IPv4:', ip)
-      return ip
+    if (isIPv4(ip) && !ipv4) {
+      ipv4 = ip
+    } else if (isIPv6(ip) && !ipv6) {
+      ipv6 = ip
     }
   }
   
-  // Pokud nenajdeme IPv4, vezmeme první dostupnou
-  const fallbackIp = validIps[0] || 'unknown'
-  console.log('Fallback IP:', fallbackIp)
-  return fallbackIp
+  // Vytvoříme objekt s daty
+  const ipData = {
+    primary: ipv4 || ipv6 || 'unknown', // IPv4 prioritně
+    ipv4: ipv4,
+    ipv6: ipv6,
+    protocol: ipv4 ? 'IPv4' : ipv6 ? 'IPv6' : 'unknown'
+  }
+  
+  console.log('IP Data:', ipData)
+  return ipData
 }
 
 function isIPv4(ip) {
@@ -98,15 +108,35 @@ function isIPv4(ip) {
   return ipv4Regex.test(ip)
 }
 
-function transform(html, ip, lang) {
+function isIPv6(ip) {
+  // Kontrola IPv6 formátu
+  const ipv6Regex = /^(([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])|([0-9a-fA-F]{1,4}:){1,4}:((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9]))$/
+  return ipv6Regex.test(ip)
+}
+
+function transform(html, ipData, lang) {
   const translationLines = translations[lang].split('\n')
   html = html
     .replace('{{LANG}}', lang)
     .replaceAll(/\{\{LINE([0-9]+)\}\}/g, (all, p1) => {
       return translationLines[p1 - 1]
     })
-    .replaceAll('{{IP}}', ip)
-    .replace(' copyElement.value.length', ip.length)
+    .replaceAll('{{IP}}', ipData.primary)
+    .replace(' copyElement.value.length', ipData.primary.length)
+  
+  // Přidáme podporu pro zobrazení obou IP adres
+  if (ipData.ipv4 && ipData.ipv6) {
+    html = html.replace('{{IP_INFO}}', `
+      <div class="ip-details">
+        <p><strong>Primární IP:</strong> ${ipData.primary} (${ipData.protocol})</p>
+        <p><strong>IPv4:</strong> ${ipData.ipv4}</p>
+        <p><strong>IPv6:</strong> ${ipData.ipv6}</p>
+      </div>
+    `)
+  } else {
+    html = html.replace('{{IP_INFO}}', '')
+  }
+  
   return html
 }
 
@@ -128,12 +158,31 @@ function getLang(header) {
   return 'en'
 }
 
-function generateRandomIpv4OrIpv6Address() {
-  // 90% šance na IPv4, pouze 10% na IPv6
-  if (Math.random() < 0.9) {
-    return crypto.getRandomValues(new Uint8Array(4)).join('.')
+function generateRandomIpData() {
+  const hasIpv4 = Math.random() < 0.9 // 90% šance na IPv4
+  const hasIpv6 = Math.random() < 0.6 // 60% šance na IPv6
+  
+  let ipv4 = null
+  let ipv6 = null
+  
+  if (hasIpv4) {
+    ipv4 = crypto.getRandomValues(new Uint8Array(4)).join('.')
   }
-  else {
-    return Array.from(crypto.getRandomValues(new Uint16Array(8))).map((e) => e.toString(16)).join(':')
+  
+  if (hasIpv6) {
+    ipv6 = Array.from(crypto.getRandomValues(new Uint16Array(8)))
+      .map((e) => e.toString(16)).join(':')
+  }
+  
+  // Pokud nemáme ani jednu, vygenerujeme IPv4
+  if (!ipv4 && !ipv6) {
+    ipv4 = crypto.getRandomValues(new Uint8Array(4)).join('.')
+  }
+  
+  return {
+    primary: ipv4 || ipv6,
+    ipv4: ipv4,
+    ipv6: ipv6,
+    protocol: ipv4 ? 'IPv4' : 'IPv6'
   }
 }
